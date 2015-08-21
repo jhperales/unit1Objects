0aed9af28cc9a1e25d1f202f61ea2a0507efb969
jhperales <jhperales@NN123-HA-22.HS.sd203.org>
2015-08-21 14:55:18 -0500

    MORE STUFF
    I HAS ADDED MORE STUFF

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -8,6 +8,25 @@ public class TurtleWorld
         Turtle turtle = new Turtle(turtleWorld);
         turtle.setPenColor(Color.RED);
         turtle.penDown();
+        int x = 1;
+        while (true)
+        {   
+            turtle.forward(x);
+            x += 1;
+            turtle.turn(90);
+            turtle.forward(x);
+            x += 1;
+            turtle.turn(90);
+            turtle.forward(x);
+            x += 1;
+            turtle.turn(90);
+            turtle.forward(x);
+            x += 1;
+            turtle.turn(90);
+          
+            
+            
        
     }
+}
 }
